e5195502452157c7cf8d05c090f05e3cedd85178
kayalvizhi <madhukayal88@gmail.com>
2026-08-30 04:35:45 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
-const express = require('express');
-const path = require('path');
+// server.js
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Memory Game running at http://localhost:${PORT}`);
 });
